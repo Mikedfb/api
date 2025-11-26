60bc5d1cd9b6e579e35ea4c9c0e2dd9ee8fb0ec5
Mike Felix <mike.felix.d@gmail.com>
2025-11-26 00:26:51 -0300

    falha no db

--- backend-lolli-pet/src/config/database.js
+++ backend-lolli-pet/src/config/database.js
@@ -1,12 +1,18 @@
 import 'dotenv/config'
 
 export default {
-  dialect: 'mariadb',
+  // CORREÇÃO 1
+  dialect: 'mysql',
+
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
+
+  // CORREÇÃO 2: Essencial para MySQL 8.0
+  allowPublicKeyRetrieval: true,
+
   define: {
     timestamps: true,
     underscored: true,
@@ -14,8 +20,13 @@ export default {
     'createdAt': 'created_at',
     'updatedAt': 'updated_at'
   },
+
   dialectOptions: {
-    timezone: 'America/Sao_Paulo'
+    timezone: 'America/Sao_Paulo',
+    // CORREÇÃO 3
+    ssl: false
   },
-  timezone: 'America/Sao_Paulo'
+
+  timezone: 'America/Sao_Paulo',
 }
+
